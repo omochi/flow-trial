1938f47357cacc8263436f4ed6154567ce3a1a34
omochimetaru <omochi.metaru@gmail.com>
2015-05-13 23:59:38 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// @flow
+var http = require("http");
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, { "Content-Type": "text/plain"});
+	res.write("Hello World");
+	res.end();
+});
+server.listen(5000);
